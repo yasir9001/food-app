4f5a2c5a42419e045c90d9e9133a7a28cf3e9987
Yasir Hussain <yasir9001@yahoo.com>
2019-06-17 20:03:15 +0500

    infinite scroll implemented

--- src/components/user-view/user-view-restaurant/UserViewRestaurants.js
+++ src/components/user-view/user-view-restaurant/UserViewRestaurants.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Input, Tag } from 'antd';
+import { List, message, Avatar, Spin } from 'antd';
+import reqwest from 'reqwest';
 
+import InfiniteScroll from 'react-infinite-scroller';
 
 const Search = Input.Search;
 const CheckableTag = Tag.CheckableTag;
@@ -58,8 +61,118 @@ export class UserViewRestaurants extends Component {
                         </div>
                     </div>
 
+                    <div>
+                        <InfiniteListExample />
+                    </div>
+
+
+
                 </div>
             </div>
         )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const fakeDataUrl = 'https://randomuser.me/api/?results=20&inc=name,gender,email,nat&noinfo';
+
+class InfiniteListExample extends React.Component {
+    state = {
+        data: [],
+        loading: false,
+        hasMore: true,
+        stopData: false
+    };
+
+    componentDidMount() {
+        this.fetchData(res => {
+            this.setState({
+                data: res.results,
+            });
+        });
+
+        window.addEventListener('scroll', (event) => {
+            if (document.body.scrollHeight <= document.documentElement.scrollTop + window.innerHeight) this.handleInfiniteOnLoad()
+        });
+    }
+
+    fetchData = callback => {
+        reqwest({
+            url: fakeDataUrl,
+            type: 'json',
+            method: 'get',
+            contentType: 'application/json',
+            success: res => {
+                callback(res);
+            },
+        });
+    };
+
+    handleInfiniteOnLoad = () => {
+        let data = this.state.data;
+        this.setState({
+            loading: true,
+        });
+        if (data.length > 20) {
+            message.warning('End of search result');
+            this.setState({
+                hasMore: false,
+                loading: false,
+            });
+            return;
+        }
+        this.fetchData(res => {
+            data = data.concat(res.results);
+            this.setState({
+                data,
+                loading: false,
+            });
+        });
+    };
+
+    render() {
+
+        return (
+            <div className="demo-infinite-container">
+                <InfiniteScroll
+                    initialLoad={false}
+                    pageStart={0}
+                    loadMore={this.handleInfiniteOnLoad}
+                    hasMore={!this.state.loading && this.state.hasMore}
+                    useWindow={false}
+                >
+                    <List
+                        dataSource={this.state.data}
+                        renderItem={item => (
+                            <List.Item key={item.id}>
+                                <List.Item.Meta
+                                    avatar={
+                                        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                                    }
+                                    title={<a href="">{item.name.last}</a>}
+                                    description={item.email}
+                                />
+                            </List.Item>
+                        )}
+                    >
+                        {this.state.loading && this.state.hasMore && (
+                            <div className="demo-loading-container">
+                                <Spin />
+                            </div>
+                        )}
+                    </List>
+                </InfiniteScroll>
+            </div>
+        );
     }
 }
